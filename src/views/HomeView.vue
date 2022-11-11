fc50b4bc6bbeaccd0d03201bb1476a44785c30f4
llthyy <43698043+llthyy@users.noreply.github.com>
2022-11-11 14:53:41 +0800

    Add files via upload
    初始化

---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+  <RouterLink to="/map">Map</RouterLink>
+  <br/>
+  <RouterLink to="/about">About</RouterLink>
+</template>
+
+<script setup></script>
+<style scoped>
+#map-container {
+  height: 200px;
+  width: 100%;
+}
+</style>
---
+++ src/views/MapView.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="map-container"></div>
+</template>
+
+<script setup>
+import { onMounted } from "vue";
+
+let map = null;
+
+function initMap(mapElement = "map-container") {
+  const mapOptions = {
+    // viewMode: '3D', //使用3D视图
+    zoom: 12,
+    zooms: [2, 22],
+  };
+  AMapLoader.load({
+    key: "490579eee171cd7c97c9aff423e3fda5",
+    version: "2.0", // 默认为 1.4.15
+    plugins: [
+      "AMap.ToolBar",
+      "AMap.Scale",
+      "AMap.MarkerCluster",
+      "AMap.MoveAnimation",
+    ],
+  }).then((AMap) => {
+    map = new AMap.Map(mapElement, mapOptions);
+    map.on("complete", function () {
+      // 地图图块加载完成后触发
+      console.log("地图图块加载完成");
+    });
+  });
+}
+
+onMounted(() => {
+  initMap();
+});
+</script>
+<style scoped>
+#map-container {
+  height: 100vh;
+  width: 100vh;
+}
+</style>
